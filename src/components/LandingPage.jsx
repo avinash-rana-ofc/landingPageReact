fb569d92cc80bbdc38af61f3c5c0254c0db585a3
Avinash <avinashrana55@yahoo.com>
2025-01-09 23:49:24 +0530

    Added Landing Page, modified Navbar and updated App
    LandingPage Component is created and added to App component and navbar font updated properly

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
+        <div className="textstructure mt-40 px-20">
+            <div className="masker">
+                <h1 className="uppercase text-9xl leading-[4vw] font-['Test_Founders_Grotesk_X-Cond_SmBd']">DOE</h1>
+            </div>
+            <div className="masker">
+                <h1 className="uppercase text-9xl leading-none font-['Test_Founders_Grotesk_X-Cond_SmBd']">Directorate of Electricity</h1>
+            </div>
+            <div className="masker">
+                <h1 className="uppercase text-9xl leading-none font-['Test_Founders_Grotesk_X-Cond_SmBd']">Power</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import Logo from "../assets/logo.svg"
 
 function Navbar() {
   return (
-    <div className="w-full px-20 py-8 font-['Neue Montreal'] flex justify-between items-center">
+    <div className="fixed z-[999] w-full px-20 py-8 font-['Neue_Montreal'] flex justify-between items-center">
       <div className="logo">
         <img src={Logo} className="w-20 h-20"/>
       </div>
       <div className="links flex gap-10">
         {["Home", "Services", "about us", "Reports", "Contacts"].map((li, i) => {
-          return <a className={`text-md capitalize font-light ${i == 4 ? 'ml-10':''}` }key={i} >{li}</a>
+          return <a className={`text-lg capitalize font-light ${i == 4 && 'ml-32'}` }key={i} >{li}</a>
         })}
       </div>
     </div>
