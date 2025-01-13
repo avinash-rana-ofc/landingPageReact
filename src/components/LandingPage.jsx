aea4e2c698e1f959ddcf50ec7eaf24f2cd5b78eb
Avinash <avinashrana55@yahoo.com>
2025-01-13 23:42:15 +0530

    Added Landing Page Contents
    modified landingpage contents using map as contents div are repeating

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,7 +4,16 @@ function LandingPage() {
   return (
     <div className='w-full h-screen bg-zinc-900 pt-1'>
         <div className="textstructure mt-40 px-20">
-            <div className="masker">
+            {["DOE", "Directorate of Electricity","Power"].map((item, index) => {
+                return (
+                        <div className="masker" key={index}>
+                            <h1 className="uppercase text-9xl leading-[6.5vw] font-['Test_Founders_Grotesk_X-Cond_SmBd']">{item}</h1>
+                        </div>
+                        );
+                
+            })
+            }
+            {/* <div className="masker">
                 <h1 className="uppercase text-9xl leading-[4vw] font-['Test_Founders_Grotesk_X-Cond_SmBd']">DOE</h1>
             </div>
             <div className="masker">
@@ -12,7 +21,7 @@ function LandingPage() {
             </div>
             <div className="masker">
                 <h1 className="uppercase text-9xl leading-none font-['Test_Founders_Grotesk_X-Cond_SmBd']">Power</h1>
-            </div>
+            </div> */}
         </div>
     </div>
   )
