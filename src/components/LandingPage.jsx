41e53dfceac5f54402d5fce3265b2ee205cfecb6
Avinash <avinashrana55@yahoo.com>
2025-01-15 18:39:30 +0530

    Modified LandingPage and adde React icons

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaArrowUp } from "react-icons/fa";
 
 function LandingPage() {
   return (
@@ -7,21 +8,29 @@ function LandingPage() {
             {["DOE", "Directorate of Electricity","Power"].map((item, index) => {
                 return (
                         <div className="masker" key={index}>
-                            <h1 className="uppercase text-9xl leading-[6.5vw] font-['Test_Founders_Grotesk_X-Cond_SmBd']">{item}</h1>
+                            <div className="w-fit flex items-center" >
+                                <h1 className="flex items-center uppercase text-[9vw] leading-[7vw] font-['Founders_Grotesk_X-Condensed'] font-bold">{item}</h1>
+                            </div>
                         </div>
+
                         );
-                
+                    })
+            }
+            
+        </div>
+        <div className="border-t-[1px] border-zinc-700 mt-32 flex justify-between items-center py-5 px-20">
+            {["For citizen and officials use only", "Test"].map((list, index) => {
+                return <p key={index} className='text-md font-light tracking-tight leading-none'>{list}</p>
             })
             }
-            {/* <div className="masker">
-                <h1 className="uppercase text-9xl leading-[4vw] font-['Test_Founders_Grotesk_X-Cond_SmBd']">DOE</h1>
+            <div className="start flex items-center gap-5">
+                <div className="px-5 py-2 border-[1px] border-zinc-400 font-light text-md rounded-full uppercase" >Apply</div>
+                <div className="w-10 h-10 border-[2px] flex items-center justify-center border-zinc-500 rounded-full">
+                    <span className='rotate-[45deg]'>
+                        <FaArrowUp />
+                    </span>
+                </div>
             </div>
-            <div className="masker">
-                <h1 className="uppercase text-9xl leading-none font-['Test_Founders_Grotesk_X-Cond_SmBd']">Directorate of Electricity</h1>
-            </div>
-            <div className="masker">
-                <h1 className="uppercase text-9xl leading-none font-['Test_Founders_Grotesk_X-Cond_SmBd']">Power</h1>
-            </div> */}
         </div>
     </div>
   )
